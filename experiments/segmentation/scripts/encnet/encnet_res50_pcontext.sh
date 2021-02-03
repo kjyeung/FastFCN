4de92fc0d5809592e6998e7a4c47a1d21a929739
Kyle Yeung <kayj.yang@gmail.com>
2021-02-03 15:36:44 +0800

    correct links to pretrained resnet models

--- experiments/segmentation/scripts/encnet/encnet_res50_pcontext.sh
+++ experiments/segmentation/scripts/encnet/encnet_res50_pcontext.sh
@@ -2,30 +2,30 @@
 
 #train
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m experiments.segmentation.train --dataset pcontext \
-    --model psp --jpu --aux --aux-weight 0.4 --no-val \
-    --backbone resnet50 --checkname psp_res50_pcontext
+    --model encnet --jpu --aux --se-loss --no-val \
+    --backbone resnet50 --checkname encnet_res50_pcontext
 
 #test [single-scale]
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m experiments.segmentation.test --dataset pcontext \
-    --model psp --jpu --aux \
+    --model encnet --jpu --aux --se-loss \
     --backbone resnet50 --resume {MODEL} --split val --mode testval
 
 #test [multi-scale]
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m experiments.segmentation.test --dataset pcontext \
-    --model psp --jpu --aux \
+    --model encnet --jpu --aux --se-loss \
     --backbone resnet50 --resume {MODEL} --split val --mode testval --ms
 
 #predict [single-scale]
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m experiments.segmentation.test --dataset pcontext \
-    --model psp --jpu --aux \
+    --model encnet --jpu --aux --se-loss \
     --backbone resnet50 --resume {MODEL} --split val --mode test
 
 #predict [multi-scale]
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m experiments.segmentation.test --dataset pcontext \
-    --model psp --jpu --aux \
+    --model encnet --jpu --aux --se-loss \
     --backbone resnet50 --resume {MODEL} --split val --mode test --ms
 
 #fps
-CUDA_VISIBLE_DEVICES=0 python -m experiments.segmentation.test_fps_params --dataset pcontext \
-    --model psp --jpu --aux \
+CUDA_VISIBLE_DEVICES=6 python -m experiments.segmentation.test_fps_params --dataset pcontext \
+    --model encnet --jpu --aux --se-loss \
     --backbone resnet50
