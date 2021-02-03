9362fabb73f124e65892d21c0583316995359ae1
Kyle Yeung <kayj.yang@gmail.com>
2021-02-03 17:10:15 +0800

    add encnet scripts for cityscapes dataset

---
+++ experiments/segmentation/scripts/encnet/encnet_res101_citys.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#train
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m experiments.segmentation.train --dataset citys \
+    --model encnet --jpu --aux --se-loss --no-val \
+    --backbone resnet101 --checkname encnet_res50_citys
+
+#test [single-scale]
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m experiments.segmentation.test --dataset citys \
+    --model encnet --jpu --aux --se-loss \
+    --backbone resnet101 --resume {MODEL} --split val --mode testval
+
+#test [multi-scale]
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m experiments.segmentation.test --dataset citys \
+    --model encnet --jpu --aux --se-loss \
+    --backbone resnet101 --resume {MODEL} --split val --mode testval --ms
+
+#predict [single-scale]
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m experiments.segmentation.test --dataset citys \
+    --model encnet --jpu --aux --se-loss \
+    --backbone resnet101 --resume {MODEL} --split val --mode test
+
+#predict [multi-scale]
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m experiments.segmentation.test --dataset citys \
+    --model encnet --jpu --aux --se-loss \
+    --backbone resnet101 --resume {MODEL} --split val --mode test --ms
+
+#fps
+CUDA_VISIBLE_DEVICES=6 python -m experiments.segmentation.test_fps_params --dataset citys \
+    --model encnet --jpu --aux --se-loss \
+    --backbone resnet101
